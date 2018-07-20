3d73cc52b9b229176288fb29073fe1d69309f0bb
Hasibul Alam Ratul <haratul@gmail.com>
2018-07-21 01:00:08 +0600

    Des half working

--- DES/CipherDES.py
+++ DES/CipherDES.py
@@ -1,20 +1,33 @@
 from Crypto.Cipher import  DES
-import binascii
+import base64
 
-key = "hello123"
+keyfile = open ("key.txt","r")
+key = keyfile.read()
 
 def pad(text):
     while len(text) % 8 != 0:
         text += ' '
     return text
 
-
 des = DES.new(key, DES.MODE_ECB)
-text = "Python is the best !"
-paddedtxt = pad(text)
-ciphertxt = des.encrypt(paddedtxt)
-encrypedtxt = str(binascii.hexlify(ciphertxt),'utf-8')
-print (encrypedtxt)
 
-a=str(des.decrypt(encrypedtxt))
-print (a)
+
+choice = input("Press 'e' to Encrypt : \nPress 'd' to Decrypt : \n")
+
+if choice == 'e' :
+    filer = open("input.txt","r")
+    filew = open("cipher.txt","w")
+    plaintxt = filer.read()
+    paddedtxt = pad(plaintxt)
+    ciphertxt = des.encrypt(paddedtxt)
+    encrypt = str(base64.b64encode(ciphertxt))
+    filew.write(encrypt)
+
+elif choice == 'd' :
+    filer = open("cipher.txt","r")
+    filew = open("plain.txt","w")
+    ciphertxt = filer.read()
+    paddedtxt = pad(ciphertxt)
+    decipher = des.decrypt(paddedtxt)
+    decrypt =str(base64.b64decode(decipher))
+    filew.write(decrypt)
