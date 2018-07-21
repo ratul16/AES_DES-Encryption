513887106bef046ca2286cc341d00effb2c51de4
Hasibul Alam Ratul <haratul@gmail.com>
2018-07-21 12:37:42 +0600

    AES working git add .!

--- AES/AESCipher.py
+++ AES/AESCipher.py
@@ -1,4 +1,6 @@
 import base64
+import hashlib
+
 from Crypto import Random
 from Crypto.Cipher import AES
 
@@ -10,7 +12,7 @@ unpad = lambda s : s[0:-s[-1]]
 class AESCipher:
 
     def __init__( self, key ):
-        self.key = key 
+        self.key = hashlib.sha256(key.encode('utf-8')).digest()
 
     def encrypt( self, raw ):
         raw = pad(raw)
@@ -24,23 +26,24 @@ class AESCipher:
         cipher = AES.new(self.key, AES.MODE_CBC, iv )
         return unpad(cipher.decrypt( enc[16:] ))
 
-key = open ("key.txt","r")
+key = open("key.txt",'r')
 cipher = AESCipher(key.read())
+#a =  key.read()
+#print(a)
 
 
 choice = input("Press 'e' to Encrypt : \nPress 'd' to Decrypt : \n")
-
-
 if choice == 'e' :
-    filer = open("input.txt","r")
-    filew = open("cipher.txt","w")
-    plaintxt = filer.read()
-    encrypttxt = str(cipher.encrypt(plaintxt))
-    filew.write(encrypttxt)
+    filer = open("input.txt",'r')
+    filew = open("cipher.txt",'w')
+    inputtxt = filer.read()
+    encrypted = cipher.encrypt(inputtxt)
+    filew.write(str(encrypted))
 
 elif choice == 'd' :
-    filer = open("cipher.txt","r")
-    filew = open("plaintxt.txt","w")
-    ciphertxt = filer.read()
-    decrypttxt = str(cipher.decrypt(ciphertxt))
-    filew.write(decrypttxt)
+    filer = open("input.txt",'r')
+    filew = open("plain.txt",'w')
+    inputtxt = filer.read()
+    encrypted = cipher.encrypt(inputtxt)
+    decrypted = cipher.decrypt(encrypted)
+    filew.write(str(decrypted))
